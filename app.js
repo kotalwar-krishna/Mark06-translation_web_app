6bc2d76a2eb40e861bee44e63400a09811aa9fdf
Krishna_Kotalwar <sanketkotalwar@gmail.com>
2021-08-27 19:45:11 +0530

    input output done

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector('#textarea-input')
-console.log(txtInput)
+var outputDiv = document.querySelector("#output")
+
+//outputDiv.innerText = "Krishna Kotalwar"
+
+//console.log(txtInput)
 
 //console.log(btnTranslate)
+//"input[name = 'translator']"
 
 function clickEventHandler(){
-    console.log("Clicked!")
-    console.log("input ", txtInput.value)
+    //console.log("Clicked!")
+    //console.log("input ", txtInput.value)
+
+    outputDiv.innerText = "you written: " + txtInput.value
 }
 
 btnTranslate.addEventListener("click", clickEventHandler)
